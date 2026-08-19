259c38480449bc487630da5f1cd85849feade173
Veli HASAR <vhas_ar@hotmail.com>
2026-08-19 14:46:27 +0300

    Update BaseEntity with audit and soft delete properties from ImarAs

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -1,8 +1,29 @@
-﻿namespace Core.Entities
+using System;
+
+namespace Core.Entities
 {
-    public class BaseEntity : BaseEntity<int> { }
+    public class BaseEntity : BaseEntity<int>
+    {
+    }
+
     public class BaseEntity<T> : IEntity
     {
         public virtual T Id { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public int? DeletedBy { get; set; }
+
+        public bool? IsActive { get; set; } = true;
+
+        public bool? IsDeleted { get; set; } = false;
     }
 }
